97bc6faecfc76f097d3de0c8b41a22a0485bb773
gowripurushothaman <gowrikalpana27@gmail.com>
2017-06-24 20:03:07 +0530

    Create vowelconsonant.c

---
+++ vowelconsonant.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char c;
+printf("enter an alphabet");
+scanf("%c",&c);
+if (c=='a'||c=='A'||c=='e'||c=='E'||c=='i'||c=='I'||c=='o'||c=='O'||c=='u'||c=='U')
+printf("%c is a vowel",c);
+else
+printf("%c is a consonanat",c);
+reeturn 0;
+}
