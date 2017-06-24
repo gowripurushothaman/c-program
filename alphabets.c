5b8d34acca793778078eac88e7100b4909fe1cba
gowripurushothaman <gowrikalpana27@gmail.com>
2017-06-24 20:33:43 +0530

    Create alphabets.c

---
+++ alphabets.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char c;
+printf("enter a alphabet");
+scanf("%c",&c);
+if((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
+printf("%c is a alphabet",c);
+else
+printf("%c is not a alphabet",c);
+return 0;
+}
